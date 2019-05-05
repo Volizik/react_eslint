7f2f2c5bbf720d6702870a8f08d6db213f36af8b
Volizik <kizilov.yura@gmail.com>
2019-05-05 21:21:51 +0300

    add rulles

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
-module.exports =  {
-    semi:  true,
-    trailingComma:  'all',
-    singleQuote:  true,
-    printWidth:  120,
-    tabWidth:  4,
+module.exports = {
+    printWidth: 120, // Specify the line length that the printer will wrap on.
+    tabWidth: 4, // Specify the number of spaces per indentation-level.
+    semi: true, // Print semicolons at the ends of statements.
+    singleQuote: true, // Use single quotes instead of double quotes.
+    jsxSingleQuote: true, // Use single quotes instead of double quotes in JSX.
+    trailingComma: 'all', // Trailing commas wherever possible (including function arguments).
+    bracketSpacing: true, // Print spaces between brackets in object literals.
+    jsxBracketSameLine: true, // Put the > of a multi-line JSX element at the end of the last line instead of being alone on the next line (does not apply to self closing elements).
+    arrowParens: 'always', // Include parentheses around a sole arrow function parameter.
 };
